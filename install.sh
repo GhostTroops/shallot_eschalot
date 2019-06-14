fff2f734c0eb2d9f85a45f7bf32819683dedd8d3
0x101 <0x101@google.local>
2019-06-14 11:15:26 +0800

    install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln `pwd`/libcrypto.so.10 /lib/x86_64-linux-gnu/libcrypto.so.10
+ln `pwd`/libssl.so.10 /lib/x86_64-linux-gnu/libssl.so.10
+ln `pwd`/shallot /usr/local/bin/shallot
+ln `pwd`/eschalot /usr/local/bin/eschalot
+chmod +x /usr/local/bin/eschalot
+chmod +x /usr/local/bin/shallot
+
